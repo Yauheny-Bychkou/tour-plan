6732fb3b5ce5d9b69de602642ff86a985a5fe205
Yauheny-Bychkou <375445433672@yandex.by>
2021-05-14 10:13:29 +0300

    create: placemark in map

--- js/main.js
+++ js/main.js
@@ -19,9 +19,25 @@ function init() {
     // Порядок по умолчанию: «широта, долгота».
     // Чтобы не определять координаты центра карты вручную,
     // воспользуйтесь инструментом Определение координат.
-    center: [36.610878, 30.558232],
+    center: [7.575227, 79.803963],
     // Уровень масштабирования. Допустимые значения:
     // от 0 (весь мир) до 19.
-    zoom: 17,
+    zoom: 12,
   });
+  myGeoObject = new ymaps.GeoObject({});
+  myMap.geoObjects.add(myGeoObject).add(
+    new ymaps.Placemark(
+      [7.575227, 79.803963],
+      {
+        balloonContentHeader: "Grand Hilton Hotel",
+        balloonContentBody: "Grand Hilton Hotel",
+        balloonContentFooter: "Grand Hilton Hotel",
+        hintContent: "Grand Hilton Hotel",
+      },
+      {
+        pseset: "islands#dotIcon",
+        iconColor: "#EC1F46",
+      }
+    )
+  );
 }
