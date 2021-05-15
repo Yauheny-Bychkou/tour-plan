ecde6f76f8f75052bbae9c7eb1167432ac8353da
Yauheny-Bychkou <375445433672@yandex.by>
2021-05-15 09:01:50 +0300

    create: rewiews section

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-const swiper = new Swiper(".swiper-container", {
+const hotellider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button--next",
-    prevEl: ".slider-button--prev",
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  spaceBetween: 20,
+});
+const reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
   },
   keyboard: {
     enabled: true,
